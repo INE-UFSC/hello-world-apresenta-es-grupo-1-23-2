b81121daef45cc497fbbc7aedc47af50109abee8
Pedro Gimenez <pehqge@gmail.com>
2023-08-10 15:47:15 -0300

    atualizado augusto

--- membro4.py
+++ membro4.py
@@ -2,4 +2,4 @@ def hello():
 	return """Olá, eu sou o Augusto Schweitzer, tenho 19 anos, gosto de jogar videogames, ver filmes e séries e sair com os amigos.
 	Tive bem pouca experiência previa com programação, em que fiz um projeto pequeno. Eu estou bastante ansioso para essa disciplina
 	porque com o front end finalmente conseguirei dar uma "cara" para os meus projetos, além de aprofundar o conhecimento nas bibliotecas
-	como Tkinter e Pygame.""" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	como Tkinter e Pygame."""
