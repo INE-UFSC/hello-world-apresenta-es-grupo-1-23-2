c3f5821fc2b771624c5501473064ba6a66868081
R0drigoSchwartz <rodrigo32003@gmail.com>
2023-08-10 15:16:31 -0300

    atualização membro3.py

--- membro3.py
+++ membro3.py
@@ -1,2 +1,6 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return """Olá, eu sou o Rodrigo Schwartz, tenho 19 anos, e sou natural de Brusque - SC. Me mudei para Florianópolis para poder realizar o curso de Ciências da Computação.
+	          Gosto de passar o tempo com a minha família e amigos, de ver filmes, séries e jogar jogos. Não tive nenhuma experiência com programação antes de iniciar o curso, 
+			  o que me deixou com um pouco de receio, porque eu sabia que o curso seria complicado. As minhas expectativas para a disciplina de POO2 são altas,
+			  pois acredito que vou ter a oportunidade de aprender muito a respeito do paradigma de Programação Orientada a Objetos, e de fato iniciar a programar os meus projetos."""
+			
